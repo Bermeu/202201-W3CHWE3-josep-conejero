39e14e349a4a6db59aa72bb8197ed9736ce50688
Josep Conejero <bermeu@gmail.com>
2022-01-30 12:56:03 +0100

    se modifica index.js quitándoles los comentarios

--- public/index.js
+++ public/index.js
@@ -1,5 +1,4 @@
 const cors = require("cors");
-
 const jsonServer = require("json-server");
 
 const server = jsonServer.create();
@@ -9,14 +8,3 @@ const port = process.env.PORT || 4000;
 server.use(cors());
 server.use(router);
 server.listen(port);
-
-/* (async function getPokeAPI() {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon"); // fetch resuelve a una promesa
-
-  // if response.status ==
-  const pokemon = await response.json(); // (para extraer json de un body) el método json me devuelve una promesa
-  eso que no se puede("mira esto, nene", pokemon.count);
-  // debugger; // (para ver qué tiene pokemon)
-})();
- */
-// getPokeAPI();
